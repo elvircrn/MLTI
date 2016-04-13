57d5989ced96a5d1e1ebc2df894843c92925e404
elvircrn <elvircrn@gmail.com>
2016-04-13 03:14:59 +0200

    Solved #4 and #5 from Zadaca 3

--- Zadaca3Zadatak4/main.cpp
+++ Zadaca3Zadatak4/main.cpp
@@ -1,9 +1,201 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
 
+#define LAZY 20
+
+using Edge = pair <int, int>;
+using Graph = vector <Edge>;
+
+template <class T>
+	using Matrix = std::vector <std::vector <T> >;
+
+template <class T>
+Matrix<T> createMatrix (int m, int n)
+{
+	return Matrix<T> (m, vector <T> (n));
+}
+
+template <class T>
+void printMatrix (Matrix <T> mat)
+{
+	int n = mat.size(), m = mat [0].size();
+	for (int i = 0; i < n; i++, cout << endl)
+		for (int j = 0; j < m; j++)
+			cout << mat [i] [j] << ' ';
+	return;
+}
+
+void printGraph(Graph g)
+{
+	for (auto e : g)
+		cout << e.first << ' ' << e.second << endl;
+}
+
+Matrix<bool> composition (Matrix <bool> a, Matrix<bool> b)
+{
+
+	auto ret = createMatrix<bool>(a.size(), a.size());
+	for (int i = 0; i < a.size(); i++)
+		for (int j = 0; j < a.size(); j++)
+			for (int k = 0; k < a.size(); k++)
+				if (a [i] [j] & b [j] [k])
+					ret [i] [k] = true;
+	return ret;
+}
+
+void relate(vector <vector <bool> > &mat, vector <int> r)
+{
+	for (int &x : r)
+		x--;
+	for (int i = 0; i < r.size(); i++)
+	{
+		for (int j = 0; j < r.size(); j++)
+		{
+			mat [r [i]] [r [j]] = true;
+			mat [r [j]] [r [i]] = true;
+		}
+	}
+}
+
+Matrix<bool> powerMe(const Matrix<bool> &a)
+{
+	auto ret = a;
+	int n = a.size();
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			for (int k = 0; k < n; k++)
+				if (ret [i] [j] & ret [j] [k])
+					ret [i] [k] = true;
+	return ret;
+}
+
+vector <pair <int, int> > getEdge (const vector <vector <bool> > &mat)
+{
+	vector <pair <int, int> > edges;
+	int n = mat.size();
+	int m = mat [0].size();
+	for (int i = 0; i < n - 1; i++)
+		for (int j = i + 1; j < n; j++)
+			if (mat [i] [j])
+				edges.emplace_back(i + 1, j + 1);
+	return edges;
+}
+
+Matrix<bool> power (Matrix <bool> a, int p)
+{
+	vector <vector <bool> > ret = a;
+	if (p == -1)
+	{
+		for (int i = 0; i < a.size() - 1; i++)
+			for (int j = 0; j < i; j++)
+			{
+				bool x = ret [i] [j];
+				bool y = ret [j] [i];
+				swap (x, y);
+
+				ret [i] [j] = x;
+				ret [j] [i] = y;
+			}
+	}
+	else
+	{
+		for (int i = 0; i < p; i++)
+			ret = composition (ret, a);
+	}
+	return ret;
+}
+
+
+Matrix<bool> plusMe(Matrix <bool> a)
+{
+	return power (a, LAZY);
+}
+
+Matrix<bool> starMe(Matrix<bool> a)
+{
+	auto ret = plusMe(a);
+	for (int i = 0; i < a [0].size(); i++)
+		a [i] [i] = true;
+	return a;
+}
+
+
+
+vector <int> getVertices (Matrix<bool> m)
+{
+	vector <int> v;
+	for (int i = 0; i < m.size(); i++)
+	{
+		for (int j = 0; j < m.size(); j++)
+		{
+			if (m [i] [j])
+			{
+				v.push_back(i + 1);
+				break;
+			}
+		}
+	}
+	return v;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+	auto R1 = createMatrix<bool> (6, 6);
+
+	relate (R1, { 3, 4, 5 });
+	relate (R1, { 1, 2 });
+	relate (R1, { 6 });
+
+	cout << "R^-1" << endl;
+	printMatrix(power(R1, -1));
+	cout << endl;
+
+	cout << "R^2" << endl;
+	printMatrix(power(R1, 2));
+	cout << endl;
+
+	cout << "R+" << endl;
+	printMatrix(plusMe(R1));
+	cout << endl;
+
+	cout << "R*" << endl;
+	printMatrix(starMe(R1));
+	cout << endl;
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
